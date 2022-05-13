e04f77df816fc28ce0c7f77e65bdaa6e7aec9769
bruvzg <7645683+bruvzg@users.noreply.github.com>
2022-05-13 15:42:07 +0300

    ﻿[Windows] Enable crash reporter on MinGW builds, use libbacktrace to generate stack trace from DWARF symbols.

--- platform/windows/detect.py
+++ platform/windows/detect.py
@@ -278,6 +278,9 @@ def configure_msvc(env, manual_msvc_config):
         env.AppendUnique(CPPDEFINES=["GLES3_ENABLED"])
         LIBS += ["opengl32"]
 
+    if env["target"] == "debug" or env["target"] == "release_debug":
+        LIBS += ["psapi", "dbghelp"]
+
     env.Append(LINKFLAGS=[p + env["LIBSUFFIX"] for p in LIBS])
 
     if manual_msvc_config:
@@ -441,12 +444,14 @@ def configure_mingw(env):
         ]
     )
 
-    env.Append(CPPDEFINES=["VULKAN_ENABLED"])
-    if not env["use_volk"]:
-        env.Append(LIBS=["vulkan"])
+    if env["vulkan"]:
+        env.Append(CPPDEFINES=["VULKAN_ENABLED"])
+        if not env["use_volk"]:
+            env.Append(LIBS=["vulkan"])
 
-    env.Append(CPPDEFINES=["GLES3_ENABLED"])
-    env.Append(LIBS=["opengl32"])
+    if env["opengl3"]:
+        env.Append(CPPDEFINES=["GLES3_ENABLED"])
+        env.Append(LIBS=["opengl32"])
 
     env.Append(CPPDEFINES=["MINGW_ENABLED", ("MINGW_HAS_SECURE_API", 1)])
 
