7ef82c29474f8c93cd2496be03f89bd869caedda
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-06-11 20:33:33 -0700

    Update scripts.

--- modules/csg/csg.h
+++ modules/csg/csg.h
@@ -64,7 +64,7 @@ struct CSGBrush {
 			faces.write[i].aabb.expand_to(faces[i].vertices[2]);
 		}
 	}
-	
+
 	// Create a brush from faces.
 	void build_from_faces(const Vector<Vector3> &p_vertices, const Vector<Vector2> &p_uvs, const Vector<bool> &p_smooth, const Vector<Ref<Material>> &p_materials, const Vector<bool> &p_invert_faces);
 	void copy_from(const CSGBrush &p_brush, const Transform3D &p_xform);
