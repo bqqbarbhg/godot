93398769defe3c5a084e1053a74049035f898ef7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-11-03 10:20:34 -0700

    Revert "Remove jiggle and fabrik."
    This reverts commit f674a5ce0458502a59ca3b750a5a96eda82cc1b9.

--- scene/2d/skeleton_modification_2d_fabrik.h
+++ scene/2d/skeleton_modification_2d_fabrik.h
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  skeleton_modification_2d_physicalbones.h                             */
+/*  skeleton_modification_2d_fabrik.h                                    */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,48 +28,72 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#ifndef SKELETON_MODIFICATION_2D_PHYSICALBONES_H
-#define SKELETON_MODIFICATION_2D_PHYSICALBONES_H
+#ifndef SKELETON_MODIFICATION_2D_FABRIK_H
+#define SKELETON_MODIFICATION_2D_FABRIK_H
 
-#include "scene/2d/physical_bone_2d.h"
 #include "scene/2d/skeleton_2d.h"
 #include "scene/2d/skeleton_modification_2d.h"
 
 ///////////////////////////////////////
-// SkeletonModification2DJIGGLE
+// SkeletonModification2DFABRIK
 ///////////////////////////////////////
 
-class SkeletonModification2DPhysicalBones : public SkeletonModification2D {
-	GDCLASS(SkeletonModification2DPhysicalBones, SkeletonModification2D);
+class SkeletonModification2DFABRIK : public SkeletonModification2D {
+	GDCLASS(SkeletonModification2DFABRIK, SkeletonModification2D);
 
 private:
-	struct PhysicalBone_Data2D {
-		NodePath physical_bone_node;
-		mutable Variant physical_bone_node_cache;
-	};
-	Vector<PhysicalBone_Data2D> physical_bone_chain;
+	struct FABRIK_Joint_Data2D {
+		NodePath bone_node;
+		mutable Variant bone_node_cache;
 
-#ifdef TOOLS_ENABLED
-	void _fetch_physical_bones();
-#endif
+		// Unlike in 3D, we need a vector of Transform2D objects to perform FABRIK.
+		// This is because FABRIK (unlike CCDIK) needs to operate on transforms that are NOT
+		// affected by each other, making the transforms stored in Bone2D unusable, as well as those in Skeleton2D.
+		// For this reason, this modification stores a vector of Transform2Ds used for the calculations, which are then applied at the end.
+		Transform2D global_transform;
+
+		Vector2 magnet_position = Vector2(0, 0);
+		bool use_target_rotation = false;
+	};
+
+	Vector<FABRIK_Joint_Data2D> fabrik_data_chain;
+
+	NodePath target_node;
+	mutable Variant target_node_cache;
+
+	float chain_tolarance = 0.01;
+	int chain_max_iterations = 10;
+	int chain_iterations = 0;
+	Transform2D target_global_pose = Transform2D();
+	Transform2D origin_global_pose = Transform2D();
+
+	void chain_backwards();
+	void chain_forwards();
 
 protected:
 	static void _bind_methods();
-	bool _get(const StringName &p_path, Variant &r_ret) const;
 	bool _set(const StringName &p_path, const Variant &p_value);
+	bool _get(const StringName &p_path, Variant &r_ret) const;
 	void _get_property_list(List<PropertyInfo> *p_list) const;
-	void execute(real_t p_delta) override;
+	void execute(real_t delta) override;
 	PackedStringArray get_configuration_warnings() const override;
 
 public:
+	void set_target_node(const NodePath &p_target_node);
+	NodePath get_target_node() const;
+
 	int get_joint_count();
 	void set_joint_count(int p_new_length);
 
-	void set_physical_bone_node(int p_joint_idx, const NodePath &p_path);
-	NodePath get_physical_bone_node(int p_joint_idx) const;
+	void set_joint_bone_node(int p_joint_idx, const NodePath &p_target_node);
+	NodePath get_joint_bone_node(int p_joint_idx) const;
+	void set_joint_magnet_position(int p_joint_idx, Vector2 p_magnet_position);
+	Vector2 get_joint_magnet_position(int p_joint_idx) const;
+	void set_joint_use_target_rotation(int p_joint_idx, bool p_use_target_rotation);
+	bool get_joint_use_target_rotation(int p_joint_idx) const;
 
-	SkeletonModification2DPhysicalBones();
-	~SkeletonModification2DPhysicalBones();
+	SkeletonModification2DFABRIK();
+	~SkeletonModification2DFABRIK();
 };
 
-#endif // SKELETON_MODIFICATION_2D_PHYSICALBONES_H
+#endif // SKELETON_MODIFICATION_2D_FABRIK_H
