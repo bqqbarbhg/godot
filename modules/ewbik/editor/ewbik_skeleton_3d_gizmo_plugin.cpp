4a77c53f1a6542501d89858a83a932f78eff30a1
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-11-09 20:40:31 -0800

    Cleanup code.

--- modules/ewbik/editor/ewbik_skeleton_3d_gizmo_plugin.cpp
+++ modules/ewbik/editor/ewbik_skeleton_3d_gizmo_plugin.cpp
@@ -89,179 +89,7 @@ void EWBIK3DGizmoPlugin::redraw(EditorNode3DGizmo *p_gizmo) {
 		}
 		Vector<int> bones_to_process = ewbik_skeleton->get_parentless_bones();
 		kusudama_shader.instantiate();
-		kusudama_shader->set_code(R"(
-// Skeleton 3D gizmo kusudama constraint shader.
-shader_type spatial;
-render_mode depth_prepass_alpha, cull_disabled;
-
-uniform vec4 kusudama_color : source_color = vec4(0.58039218187332, 0.27058824896812, 0.00784313771874, 1.0);
-uniform int cone_count = 0;
-
-// 0,0,0 is the center of the kusudama. The kusudamas have their own bases that automatically get reoriented such that +y points in the direction that is the weighted average of the limitcones on the kusudama.
-// But, if you have a kusuduma with just 1 limit_cone, then in general that limit_cone should be 0,1,0 in the kusudama's basis unless the user has specifically specified otherwise.
-
-uniform vec4 cone_sequence[30];
-
-// This shader can display up to 30 cones (represented by 30 4d vectors)
-// Each group of 4 represents the xyz coordinates of the cone direction
-// vector in model space and the fourth element represents radius
-
-// TODO: fire 2022-05-26
-// Use a texture to store bone parameters.
-// Use the uv to get the row of the bone.
-
-varying vec3 normal_model_dir;
-varying vec4 vert_model_color;
-
-bool is_in_inter_cone_path(in vec3 normal_dir, in vec4 tangent_1, in vec4 cone_1, in vec4 tangent_2, in vec4 cone_2) {
-	vec3 c1xc2 = cross(cone_1.xyz, cone_2.xyz);
-	float c1c2dir = dot(normal_dir, c1xc2);
-
-	if (c1c2dir < 0.0) {
-		vec3 c1xt1 = cross(cone_1.xyz, tangent_1.xyz);
-		vec3 t1xc2 = cross(tangent_1.xyz, cone_2.xyz);
-		float c1t1dir = dot(normal_dir, c1xt1);
-		float t1c2dir = dot(normal_dir, t1xc2);
-
-	 	return (c1t1dir > 0.0 && t1c2dir > 0.0);
-
-	} else {
-		vec3 t2xc1 = cross(tangent_2.xyz, cone_1.xyz);
-		vec3 c2xt2 = cross(cone_2.xyz, tangent_2.xyz);
-		float t2c1dir = dot(normal_dir, t2xc1);
-		float c2t2dir = dot(normal_dir, c2xt2);
-
-		return (c2t2dir > 0.0 && t2c1dir > 0.0);
-	}
-	return false;
-}
-
-//determines the current draw condition based on the desired draw condition in the setToArgument
-// -3 = disallowed entirely;
-// -2 = disallowed and on tangent_cone boundary
-// -1 = disallowed and on control_cone boundary
-// 0 =  allowed and empty;
-// 1 =  allowed and on control_cone boundary
-// 2  = allowed and on tangent_cone boundary
-int get_allowability_condition(in int current_condition, in int set_to) {
-	if((current_condition == -1 || current_condition == -2)
-		&& set_to >= 0) {
-		return current_condition *= -1;
-	} else if(current_condition == 0 && (set_to == -1 || set_to == -2)) {
-		return set_to *=-2;
-	}
-	return max(current_condition, set_to);
-}
-
-// returns 1 if normal_dir is beyond (cone.a) radians from the cone.rgb
-// returns 0 if normal_dir is within (cone.a + boundary_width) radians from the cone.rgb
-// return -1 if normal_dir is less than (cone.a) radians from the cone.rgb
-int is_in_cone(in vec3 normal_dir, in vec4 cone, in float boundary_width) {
-	float arc_dist_to_cone = acos(dot(normal_dir, cone.rgb));
-	if (arc_dist_to_cone > (cone.a+(boundary_width/2.))) {
-		return 1;
-	}
-	if (arc_dist_to_cone < (cone.a-(boundary_width/2.))) {
-		return -1;
-	}
-	return 0;
-}
-
-// Returns a color corresponding to the allowability of this region,
-// or otherwise the boundaries corresponding
-// to various cones and tangent_cone.
-vec4 color_allowed(in vec3 normal_dir,  in int cone_counts, in float boundary_width) {
-	int current_condition = -3;
-	if (cone_counts == 1) {
-		vec4 cone = cone_sequence[0];
-		int in_cone = is_in_cone(normal_dir, cone, boundary_width);
-		bool is_in_cone = in_cone == 0;
-		if (is_in_cone) {
-			in_cone = -1;
-		} else {
-			if (in_cone < 0) {
-				in_cone = 0;
-			} else {
-				in_cone = -3;
-			}
-		}
-		current_condition = get_allowability_condition(current_condition, in_cone);
-	} else {
-		for(int i=0; i < (cone_counts-1)*3; i=i+3) {
-			normal_dir = normalize(normal_dir);
-
-			vec4 cone_1 = cone_sequence[i+0];
-			vec4 tangent_1 = cone_sequence[i+1];
-			vec4 tangent_2 = cone_sequence[i+2];
-			vec4 cone_2 = cone_sequence[i+3];
-
-			int inCone1 = is_in_cone(normal_dir, cone_1, boundary_width);
-			if (inCone1 == 0) {
-				inCone1 = -1;
-			} else {
-				if (inCone1 < 0) {
-					inCone1 = 0;
-				} else {
-					inCone1 = -3;
-				}
-			}
-			current_condition = get_allowability_condition(current_condition, inCone1);
-
-			int inCone2 = is_in_cone(normal_dir, cone_2, boundary_width);
-			if (inCone2 == 0) {
-				inCone2 = -1;
-			} else {
-				if (inCone2 < 0) {
-					inCone2 = 0;
-				} else {
-					inCone2 = -3;
-				}
-			}
-			current_condition = get_allowability_condition(current_condition, inCone2);
-
-			int in_tan_1 = is_in_cone(normal_dir, tangent_1, boundary_width);
-			int in_tan_2 = is_in_cone(normal_dir, tangent_2, boundary_width);
-
-			if (float(in_tan_1) < 1. || float(in_tan_2) < 1.) {
-				in_tan_1 = in_tan_1 == 0 ? -2 : -3;
-				current_condition = get_allowability_condition(current_condition, in_tan_1);
-				in_tan_2 = in_tan_2 == 0 ? -2 : -3;
-				current_condition = get_allowability_condition(current_condition, in_tan_2);
-			} else {
-				bool in_intercone = is_in_inter_cone_path(normal_dir, tangent_1, cone_1, tangent_2, cone_2);
-				int intercone_condition = in_intercone ? 0 : -3;
-				current_condition = get_allowability_condition(current_condition, intercone_condition);
-			}
-		}
-	}
-	vec4 result = vert_model_color;
-	if (current_condition != 0 && current_condition != 2) {
-		float on_cone_boundary = current_condition == 1 ? -0.3 : 0.0;
-		result += vec4(0.0, on_cone_boundary, 0, 0.0);
-	} else {
-		return vec4(0.0, 0.0, 0.0, 0.0);
-	}
-	return result;
-}
-
-void vertex() {
-	normal_model_dir = CUSTOM0.rgb;
-	vert_model_color.rgb = kusudama_color.rgb;
-}
-
-void fragment() {
-	vec4 result_color_allowed = vec4(0.0, 0.0, 0.0, 0.0);
-	if (cone_sequence.length() == 30) {
-		result_color_allowed = color_allowed(normal_model_dir, cone_count, 0.02);
-	}
-	ALBEDO = kusudama_color.rgb;
-	if (result_color_allowed.a == 0.0) {
-		discard;
-	}
-	ALBEDO = result_color_allowed.rgb;
-	ALPHA = result_color_allowed.a;
-}
-)");
+		kusudama_shader->set_code(EWBIK_KUSUDAMA_SHADER);
 		int bones_to_process_i = 0;
 		Vector<BoneId> processing_bones;
 		Ref<IKBoneSegment> bone_segment = ewbik->get_segmented_skeleton();
@@ -307,7 +135,7 @@ void EWBIK3DGizmoPlugin::create_gizmo_mesh_handles(BoneId current_bone_idx, Bone
 	}
 	bones[0] = parent_idx;
 	weights[0] = 1;
-	Transform3D constraint_relative_to_the_skeleton = ewbik_skeleton->get_transform().affine_inverse() * ik_bone->get_constraint_transform()->get_global_transform();
+	Transform3D constraint_relative_to_the_skeleton = ewbik_skeleton->get_global_transform() * ik_bone->get_constraint_transform()->get_global_transform();
 	PackedFloat32Array kusudama_limit_cones;
 	Ref<IKKusudama> kusudama = ik_bone->get_constraint();
 	kusudama_limit_cones.resize(KUSUDAMA_MAX_CONES * 4);
@@ -411,7 +239,7 @@ void EWBIK3DGizmoPlugin::create_gizmo_mesh_handles(BoneId current_bone_idx, Bone
 			c.a = 0;
 			kusudama_surface_tool->set_custom(MESH_CUSTOM_0, c);
 			kusudama_surface_tool->set_normal(normals[point_i]);
-			kusudama_surface_tool->add_vertex(points[point_i]);
+			kusudama_surface_tool->add_vertex(constraint_relative_to_the_skeleton.xform(points[point_i]));
 		}
 		for (int32_t index_i : indices) {
 			kusudama_surface_tool->add_index(index_i);
@@ -423,7 +251,7 @@ void EWBIK3DGizmoPlugin::create_gizmo_mesh_handles(BoneId current_bone_idx, Bone
 		int32_t cone_count = kusudama->get_limit_cones().size();
 		kusudama_material->set_shader_parameter("cone_count", cone_count);
 		kusudama_material->set_shader_parameter("kusudama_color", current_bone_color);
-		p_gizmo->add_mesh(kusudama_surface_tool->commit(Ref<Mesh>(), RS::ARRAY_CUSTOM_RGBA_HALF << RS::ARRAY_FORMAT_CUSTOM0_SHIFT), kusudama_material, constraint_relative_to_the_skeleton, ewbik_skeleton->register_skin(ewbik_skeleton->create_skin_from_rest_transforms()));
+		p_gizmo->add_mesh(kusudama_surface_tool->commit(Ref<Mesh>(), RS::ARRAY_CUSTOM_RGBA_HALF << RS::ARRAY_FORMAT_CUSTOM0_SHIFT), kusudama_material, Transform3D(), ewbik_skeleton->register_skin(ewbik_skeleton->create_skin_from_rest_transforms()));
 		// END Create a kusudama ball visualization.
 	}
 	{
@@ -472,14 +300,14 @@ void EWBIK3DGizmoPlugin::create_gizmo_mesh_handles(BoneId current_bone_idx, Bone
 					// Draw 8 lines from the cone origin to the sides of the circle
 					cone_sides_surface_tool->set_bones(bones);
 					cone_sides_surface_tool->set_weights(weights);
-					cone_sides_surface_tool->add_vertex((center_relative_to_mesh).xform(Vector3(a.x, a.y, -d)));
+					cone_sides_surface_tool->add_vertex((constraint_relative_to_the_skeleton * center_relative_to_mesh).xform(Vector3(a.x, a.y, -d)));
 					cone_sides_surface_tool->set_bones(bones);
 					cone_sides_surface_tool->set_weights(weights);
-					cone_sides_surface_tool->add_vertex((center_relative_to_mesh).xform(Vector3()));
+					cone_sides_surface_tool->add_vertex((constraint_relative_to_the_skeleton * center_relative_to_mesh).xform(Vector3()));
 				}
 			}
 		}
-		p_gizmo->add_mesh(cone_sides_surface_tool->commit(), material_tertiary, constraint_relative_to_the_skeleton, ewbik_skeleton->register_skin(ewbik_skeleton->create_skin_from_rest_transforms()));
+		p_gizmo->add_mesh(cone_sides_surface_tool->commit(), material_tertiary, Transform3D(), ewbik_skeleton->register_skin(ewbik_skeleton->create_skin_from_rest_transforms()));
 	} // END cone
 	// TODO: Use several colors for the dots and match the color of the lines.
 	p_gizmo->add_handles(handles, get_material("handles"), Vector<int>(), true, true);
