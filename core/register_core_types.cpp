f76d53b5e82da56ef6642fc10e81e601ec5739ee
Faolan <46481567+Faolan-Rad@users.noreply.github.com>
2023-02-27 23:35:08 -0500

    LibGodot with GDExtension
    *    Refactored LibGodot to use GDExtension library
    *    Removed hash check for LibGodot functions
    *    Worked on fixing API loading levels in LibGodot
    
    Co-Authored-By: K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>

--- core/register_core_types.cpp
+++ core/register_core_types.cpp
@@ -313,40 +313,44 @@ void register_core_settings() {
 	}
 }
 
-void register_core_singletons() {
+void register_core_starting_singletons() {
 	GDREGISTER_CLASS(ProjectSettings);
+	GDREGISTER_CLASS(core_bind::special::ClassDB);
+	GDREGISTER_CLASS(core_bind::OS);
+	GDREGISTER_CLASS(core_bind::Engine);
+	GDREGISTER_CLASS(core_bind::Marshalls);
+	GDREGISTER_CLASS(core_bind::EngineDebugger);
+
+	Engine::get_singleton()->add_singleton(Engine::Singleton("ProjectSettings", ProjectSettings::get_singleton()));
+	Engine::get_singleton()->add_singleton(Engine::Singleton("ClassDB", _classdb));
+	Engine::get_singleton()->add_singleton(Engine::Singleton("OS", core_bind::OS::get_singleton()));
+	Engine::get_singleton()->add_singleton(Engine::Singleton("Engine", core_bind::Engine::get_singleton()));
+	Engine::get_singleton()->add_singleton(Engine::Singleton("Marshalls", core_bind::Marshalls::get_singleton()));
+	Engine::get_singleton()->add_singleton(Engine::Singleton("EngineDebugger", core_bind::EngineDebugger::get_singleton()));
+	Engine::get_singleton()->add_singleton(Engine::Singleton("GDExtensionManager", GDExtensionManager::get_singleton()));
+}
+
+void register_core_singletons() {
 	GDREGISTER_ABSTRACT_CLASS(IP);
 	GDREGISTER_CLASS(core_bind::Geometry2D);
 	GDREGISTER_CLASS(core_bind::Geometry3D);
 	GDREGISTER_CLASS(core_bind::ResourceLoader);
 	GDREGISTER_CLASS(core_bind::ResourceSaver);
-	GDREGISTER_CLASS(core_bind::OS);
-	GDREGISTER_CLASS(core_bind::Engine);
-	GDREGISTER_CLASS(core_bind::special::ClassDB);
-	GDREGISTER_CLASS(core_bind::Marshalls);
 	GDREGISTER_CLASS(TranslationServer);
 	GDREGISTER_ABSTRACT_CLASS(Input);
 	GDREGISTER_CLASS(InputMap);
 	GDREGISTER_CLASS(Expression);
-	GDREGISTER_CLASS(core_bind::EngineDebugger);
 	GDREGISTER_CLASS(Time);
 
-	Engine::get_singleton()->add_singleton(Engine::Singleton("ProjectSettings", ProjectSettings::get_singleton()));
 	Engine::get_singleton()->add_singleton(Engine::Singleton("IP", IP::get_singleton(), "IP"));
 	Engine::get_singleton()->add_singleton(Engine::Singleton("Geometry2D", core_bind::Geometry2D::get_singleton()));
 	Engine::get_singleton()->add_singleton(Engine::Singleton("Geometry3D", core_bind::Geometry3D::get_singleton()));
 	Engine::get_singleton()->add_singleton(Engine::Singleton("ResourceLoader", core_bind::ResourceLoader::get_singleton()));
 	Engine::get_singleton()->add_singleton(Engine::Singleton("ResourceSaver", core_bind::ResourceSaver::get_singleton()));
-	Engine::get_singleton()->add_singleton(Engine::Singleton("OS", core_bind::OS::get_singleton()));
-	Engine::get_singleton()->add_singleton(Engine::Singleton("Engine", core_bind::Engine::get_singleton()));
-	Engine::get_singleton()->add_singleton(Engine::Singleton("ClassDB", _classdb));
-	Engine::get_singleton()->add_singleton(Engine::Singleton("Marshalls", core_bind::Marshalls::get_singleton()));
 	Engine::get_singleton()->add_singleton(Engine::Singleton("TranslationServer", TranslationServer::get_singleton()));
 	Engine::get_singleton()->add_singleton(Engine::Singleton("Input", Input::get_singleton()));
 	Engine::get_singleton()->add_singleton(Engine::Singleton("InputMap", InputMap::get_singleton()));
-	Engine::get_singleton()->add_singleton(Engine::Singleton("EngineDebugger", core_bind::EngineDebugger::get_singleton()));
 	Engine::get_singleton()->add_singleton(Engine::Singleton("Time", Time::get_singleton()));
-	Engine::get_singleton()->add_singleton(Engine::Singleton("GDExtensionManager", GDExtensionManager::get_singleton()));
 	Engine::get_singleton()->add_singleton(Engine::Singleton("ResourceUID", ResourceUID::get_singleton()));
 	Engine::get_singleton()->add_singleton(Engine::Singleton("WorkerThreadPool", worker_thread_pool));
 }
--- main/main.cpp
+++ main/main.cpp
@@ -106,6 +106,10 @@
 #endif // DISABLE_DEPRECATED
 #endif // TOOLS_ENABLED
 
+#ifdef LIBRARY_ENABLED
+#include "core/libgodot/libgodot.h"
+#endif
+
 #include "modules/modules_enabled.gen.h" // For mono.
 
 #if defined(MODULE_MONO_ENABLED) && defined(TOOLS_ENABLED)
@@ -499,6 +503,7 @@ Error Main::test_setup() {
 	initialize_modules(MODULE_INITIALIZATION_LEVEL_CORE);
 	register_core_extensions();
 
+	register_core_starting_singletons();
 	register_core_singletons();
 
 	/** INITIALIZE SERVERS **/
@@ -1397,11 +1402,17 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 #ifdef TOOLS_ENABLED
 		editor = false;
 #else
-		const String error_msg = "Error: Couldn't load project data at path \"" + project_path + "\". Is the .pck file missing?\nIf you've renamed the executable, the associated .pck file should also be renamed to match the executable's name (without the extension).\n";
-		OS::get_singleton()->print("%s", error_msg.utf8().get_data());
-		OS::get_singleton()->alert(error_msg);
+#ifdef LIBRARY_ENABLED
+		if (!libgodot_is_scene_loadable()) {
+#endif
+			const String error_msg = "Error: Couldn't load project data at path \"" + project_path + "\". Is the .pck file missing?\nIf you've renamed the executable, the associated .pck file should also be renamed to match the executable's name (without the extension).\n";
+			OS::get_singleton()->print("%s", error_msg.utf8().get_data());
+			OS::get_singleton()->alert(error_msg);
 
-		goto error;
+			goto error;
+#ifdef LIBRARY_ENABLED
+		}
+#endif
 #endif
 	}
 
@@ -1409,7 +1420,15 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 	OS::get_singleton()->ensure_user_data_dir();
 
 	initialize_modules(MODULE_INITIALIZATION_LEVEL_CORE);
+	register_core_starting_singletons();
 	register_core_extensions(); // core extensions must be registered after globals setup and before display
+
+#ifdef LIBRARY_ENABLED
+	libgodot_project_settings_load(ProjectSettings::get_singleton());
+
+	// Initialize user data dir.
+	OS::get_singleton()->ensure_user_data_dir();
+#endif
 
 	ResourceUID::get_singleton()->load_from_cache(); // load UUIDs from cache.
 
@@ -1472,10 +1491,16 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 #ifdef TOOLS_ENABLED
 		if (!editor && !project_manager) {
 #endif
-			const String error_msg = "Error: Can't run project: no main scene defined in the project.\n";
-			OS::get_singleton()->print("%s", error_msg.utf8().get_data());
-			OS::get_singleton()->alert(error_msg);
-			goto error;
+#ifdef LIBRARY_ENABLED
+			if (!libgodot_is_scene_loadable()) {
+#endif
+				const String error_msg = "Error: Can't run project: no main scene defined in the project.\n";
+				OS::get_singleton()->print("%s", error_msg.utf8().get_data());
+				OS::get_singleton()->alert(error_msg);
+				goto error;
+#ifdef LIBRARY_ENABLED
+			}
+#endif
 #ifdef TOOLS_ENABLED
 		}
 #endif
@@ -2802,6 +2827,11 @@ bool Main::start() {
 				}
 				Engine::get_singleton()->startup_benchmark_end_measure(); // load autoloads
 			}
+#ifdef LIBRARY_ENABLED
+			else {
+				libgodot_scene_load((void *)sml);
+			}
+#endif
 		}
 
 #ifdef TOOLS_ENABLED
