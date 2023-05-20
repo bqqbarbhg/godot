b19fa5c8581a44ab1f29ab541fcbc446fa64a317
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-05-20 10:19:19 -0700

    Avoid duplicate symbols to link.

--- modules/vrm/vrm_collidergroup.cpp
+++ modules/vrm/vrm_collidergroup.cpp
@@ -29,3 +29,40 @@
 /**************************************************************************/
 
 #include "vrm_collidergroup.h"
+
+void SphereCollider::update(Node3D *parent, Skeleton3D *skel) {
+	if (parent->get_class() == "Skeleton3D" && idx != -1) {
+		Skeleton3D *skeleton = Object::cast_to<Skeleton3D>(parent);
+		position = VRMUtil::transform_point(Transform3D(Basis(), skeleton->get_global_transform().xform(skel->get_bone_global_pose(idx).origin)), offset);
+	} else {
+		position = VRMUtil::transform_point(parent->get_global_transform(), offset);
+	}
+}
+
+float SphereCollider::get_radius() const {
+	return radius;
+}
+
+Vector3 SphereCollider::get_position() const {
+	return position;
+}
+
+void VRMColliderGroup::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("setup"), &VRMColliderGroup::setup);
+	ClassDB::bind_method(D_METHOD("_ready", "ready_parent", "ready_skeleton"), &VRMColliderGroup::_ready);
+	ClassDB::bind_method(D_METHOD("_process"), &VRMColliderGroup::_process);
+
+	ClassDB::bind_method(D_METHOD("get_skeleton_or_node"), &VRMColliderGroup::get_skeleton_or_node);
+	ClassDB::bind_method(D_METHOD("set_skeleton_or_node", "skeleton_or_node"), &VRMColliderGroup::set_skeleton_or_node);
+	ClassDB::bind_method(D_METHOD("get_bone"), &VRMColliderGroup::get_bone);
+	ClassDB::bind_method(D_METHOD("set_bone", "bone"), &VRMColliderGroup::set_bone);
+	ClassDB::bind_method(D_METHOD("get_sphere_colliders"), &VRMColliderGroup::get_sphere_colliders);
+	ClassDB::bind_method(D_METHOD("set_sphere_colliders", "sphere_colliders"), &VRMColliderGroup::set_sphere_colliders);
+	ClassDB::bind_method(D_METHOD("get_gizmo_color"), &VRMColliderGroup::get_gizmo_color);
+	ClassDB::bind_method(D_METHOD("set_gizmo_color", "gizmo_color"), &VRMColliderGroup::set_gizmo_color);
+
+	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "skeleton_or_node"), "set_skeleton_or_node", "get_skeleton_or_node");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "bone"), "set_bone", "get_bone");
+	ADD_PROPERTY(PropertyInfo(Variant::ARRAY, "sphere_colliders"), "set_sphere_colliders", "get_sphere_colliders");
+	ADD_PROPERTY(PropertyInfo(Variant::COLOR, "gizmo_color"), "set_gizmo_color", "get_gizmo_color");
+}
--- modules/vrm/vrm_constants.cpp
+++ modules/vrm/vrm_constants.cpp
@@ -29,3 +29,73 @@
 /**************************************************************************/
 
 #include "vrm_constants.h"
+
+void VRMConstants::_bind_methods() {
+}
+
+VRMConstants::VRMConstants(bool is_vrm_0) {
+	vrm_to_human_bone.insert("hips", "Hips");
+	vrm_to_human_bone.insert("spine", "Spine");
+	vrm_to_human_bone.insert("chest", "Chest");
+	vrm_to_human_bone.insert("upperChest", "UpperChest");
+	vrm_to_human_bone.insert("neck", "Neck");
+	vrm_to_human_bone.insert("head", "Head");
+	vrm_to_human_bone.insert("leftEye", "LeftEye");
+	vrm_to_human_bone.insert("rightEye", "RightEye");
+	vrm_to_human_bone.insert("jaw", "Jaw");
+	vrm_to_human_bone.insert("leftShoulder", "LeftShoulder");
+	vrm_to_human_bone.insert("leftUpperArm", "LeftUpperArm");
+	vrm_to_human_bone.insert("leftLowerArm", "LeftLowerArm");
+	vrm_to_human_bone.insert("leftHand", "LeftHand");
+	vrm_to_human_bone.insert("leftThumbMetacarpal", "LeftThumbMetacarpal");
+	vrm_to_human_bone.insert("leftThumbProximal", "LeftThumbProximal");
+	vrm_to_human_bone.insert("leftThumbDistal", "LeftThumbDistal");
+	vrm_to_human_bone.insert("leftIndexProximal", "LeftIndexProximal");
+	vrm_to_human_bone.insert("leftIndexIntermediate", "LeftIndexIntermediate");
+	vrm_to_human_bone.insert("leftIndexDistal", "LeftIndexDistal");
+	vrm_to_human_bone.insert("leftMiddleProximal", "LeftMiddleProximal");
+	vrm_to_human_bone.insert("leftMiddleIntermediate", "LeftMiddleIntermediate");
+	vrm_to_human_bone.insert("leftMiddleDistal", "LeftMiddleDistal");
+	vrm_to_human_bone.insert("leftRingProximal", "LeftRingProximal");
+	vrm_to_human_bone.insert("leftRingIntermediate", "LeftRingIntermediate");
+	vrm_to_human_bone.insert("leftRingDistal", "LeftRingDistal");
+	vrm_to_human_bone.insert("leftLittleProximal", "LeftLittleProximal");
+	vrm_to_human_bone.insert("leftLittleIntermediate", "LeftLittleIntermediate");
+	vrm_to_human_bone.insert("leftLittleDistal", "LeftLittleDistal");
+	vrm_to_human_bone.insert("rightShoulder", "RightShoulder");
+	vrm_to_human_bone.insert("rightUpperArm", "RightUpperArm");
+	vrm_to_human_bone.insert("rightLowerArm", "RightLowerArm");
+	vrm_to_human_bone.insert("rightHand", "RightHand");
+	vrm_to_human_bone.insert("rightThumbMetacarpal", "RightThumbMetacarpal");
+	vrm_to_human_bone.insert("rightThumbProximal", "RightThumbProximal");
+	vrm_to_human_bone.insert("rightThumbDistal", "RightThumbDistal");
+	vrm_to_human_bone.insert("rightIndexProximal", "RightIndexProximal");
+	vrm_to_human_bone.insert("rightIndexIntermediate", "RightIndexIntermediate");
+	vrm_to_human_bone.insert("rightIndexDistal", "RightIndexDistal");
+	vrm_to_human_bone.insert("rightMiddleProximal", "RightMiddleProximal");
+	vrm_to_human_bone.insert("rightMiddleIntermediate", "RightMiddleIntermediate");
+	vrm_to_human_bone.insert("rightMiddleDistal", "RightMiddleDistal");
+	vrm_to_human_bone.insert("rightRingProximal", "RightRingProximal");
+	vrm_to_human_bone.insert("rightRingIntermediate", "RightRingIntermediate");
+	vrm_to_human_bone.insert("rightRingDistal", "RightRingDistal");
+	vrm_to_human_bone.insert("rightLittleProximal", "RightLittleProximal");
+	vrm_to_human_bone.insert("rightLittleIntermediate", "RightLittleIntermediate");
+	vrm_to_human_bone.insert("rightLittleDistal", "RightLittleDistal");
+	vrm_to_human_bone.insert("leftUpperLeg", "LeftUpperLeg");
+	vrm_to_human_bone.insert("leftLowerLeg", "LeftLowerLeg");
+	vrm_to_human_bone.insert("leftFoot", "LeftFoot");
+	vrm_to_human_bone.insert("leftToes", "LeftToes");
+	vrm_to_human_bone.insert("rightUpperLeg", "RightUpperLeg");
+	vrm_to_human_bone.insert("rightLowerLeg", "RightLowerLeg");
+	vrm_to_human_bone.insert("rightFoot", "RightFoot");
+	vrm_to_human_bone.insert("rightToes", "RightToes");
+	if (is_vrm_0) {
+		vrm_to_human_bone["leftThumbIntermediate"] = "LeftThumbProximal";
+		vrm_to_human_bone["leftThumbProximal"] = "LeftThumbMetacarpal";
+		vrm_to_human_bone["rightThumbIntermediate"] = "RightThumbProximal";
+		vrm_to_human_bone["rightThumbProximal"] = "RightThumbMetacarpal";
+	}
+	for (const KeyValue<String, String> &E : vrm_to_human_bone) {
+		vrm_to_human_bone_dict[E.key] = E.value;
+	}
+}
