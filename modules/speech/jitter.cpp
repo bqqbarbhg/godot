3c828cbac485dca74ee65f7f57a7547d8678a34a
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-05-23 22:24:40 -0700

    Initialize struct.

--- modules/speech/jitter.cpp
+++ modules/speech/jitter.cpp
@@ -111,34 +111,34 @@ static void tb_add(struct TimingBuffer *tb, spx_int16_t timing) {
 
 /** Jitter buffer structure */
 struct JitterBuffer_ {
-	spx_uint32_t pointer_timestamp; /**< Timestamp of what we will *get* next */
-	spx_uint32_t last_returned_timestamp; /**< Useful for getting the next packet with the same timestamp (for fragmented media) */
-	spx_uint32_t next_stop; /**< Estimated time the next get() will be called */
+	spx_uint32_t pointer_timestamp = 0U; /**< Timestamp of what we will *get* next */
+	spx_uint32_t last_returned_timestamp = 0U; /**< Useful for getting the next packet with the same timestamp (for fragmented media) */
+	spx_uint32_t next_stop = 0U; /**< Estimated time the next get() will be called */
 
-	spx_int32_t buffered; /**< Amount of data we think is still buffered by the application (timestamp units)*/
+	spx_int32_t buffered = 0; /**< Amount of data we think is still buffered by the application (timestamp units)*/
 
 	JitterBufferPacket packets[SPEEX_JITTER_MAX_BUFFER_SIZE]; /**< Packets stored in the buffer */
 	spx_uint32_t arrival[SPEEX_JITTER_MAX_BUFFER_SIZE]; /**< Packet arrival time (0 means it was late, even though it's a valid timestamp) */
 
-	void (*destroy)(void *); /**< Callback for destroying a packet */
+	void (*destroy)(void *) = nullptr; /**< Callback for destroying a packet */
 
-	spx_int32_t delay_step; /**< Size of the steps when adjusting buffering (timestamp units) */
-	spx_int32_t concealment_size; /**< Size of the packet loss concealment "units" */
-	int reset_state; /**< True if state was just reset        */
-	int buffer_margin; /**< How many frames we want to keep in the buffer (lower bound) */
-	int late_cutoff; /**< How late must a packet be for it not to be considered at all */
-	int interp_requested; /**< An interpolation is requested by speex_jitter_update_delay() */
-	int auto_adjust; /**< Whether to automatically adjust the delay at any time */
+	spx_int32_t delay_step = 0; /**< Size of the steps when adjusting buffering (timestamp units) */
+	spx_int32_t concealment_size = 0; /**< Size of the packet loss concealment "units" */
+	int reset_state = 0; /**< True if state was just reset        */
+	int buffer_margin = 0; /**< How many frames we want to keep in the buffer (lower bound) */
+	int late_cutoff = 0; /**< How late must a packet be for it not to be considered at all */
+	int interp_requested = 0; /**< An interpolation is requested by speex_jitter_update_delay() */
+	int auto_adjust = 0; /**< Whether to automatically adjust the delay at any time */
 
 	struct TimingBuffer _tb[MAX_BUFFERS]; /**< Don't use those directly */
 	struct TimingBuffer *timeBuffers[MAX_BUFFERS]; /**< Storing arrival time of latest frames so we can compute some stats */
-	int window_size; /**< Total window over which the late frames are counted */
-	int subwindow_size; /**< Sub-window size for faster computation  */
-	int max_late_rate; /**< Absolute maximum amount of late packets tolerable (in percent) */
-	int latency_tradeoff; /**< Latency equivalent of losing one percent of packets */
-	int auto_tradeoff; /**< Latency equivalent of losing one percent of packets (automatic default) */
+	int window_size = 0; /**< Total window over which the late frames are counted */
+	int subwindow_size = 0; /**< Sub-window size for faster computation  */
+	int max_late_rate = 0; /**< Absolute maximum amount of late packets tolerable (in percent) */
+	int latency_tradeoff = 0; /**< Latency equivalent of losing one percent of packets */
+	int auto_tradeoff = 0; /**< Latency equivalent of losing one percent of packets (automatic default) */
 
-	int lost_count; /**< Number of consecutive lost packets  */
+	int lost_count = 0; /**< Number of consecutive lost packets  */
 };
 
 /** Reset jitter buffer */
