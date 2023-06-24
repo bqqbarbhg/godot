ff0b644cf9434aab4c759cc6b04c2d725a74f11b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-06-24 00:04:28 -0700

    git subrepo pull modules/renik
    subrepo:
      subdir:   "modules/renik"
      merged:   "d0ed4e4b9ca"
    upstream:
      origin:   "git@github.com:V-Sekai/renik.git"
      branch:   "master"
      commit:   "d0ed4e4b9ca"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "110b9eb"

--- modules/renik/renik/renik_helper.h
+++ modules/renik/renik/renik_helper.h
@@ -1,5 +1,36 @@
-#ifndef RENIKHELPER_H
-#define RENIKHELPER_H
+/**************************************************************************/
+/*  renik_helper.h                                                        */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#ifndef RENIK_HELPER_H
+#define RENIK_HELPER_H
+
 #ifndef _3D_DISABLED
 
 #include <scene/main/node.h>
@@ -19,4 +50,5 @@ Basis log_clamp(Basis basis, Basis target, float looseness);
 } // namespace RenIKHelper
 
 #endif
-#endif
+
+#endif // RENIK_HELPER_H
