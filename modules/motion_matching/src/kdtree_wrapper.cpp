0d919dc7e53676ddf5bf7d384576d94cdf9fd300
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-07-06 20:59:19 -0700

    Extract method definition from the prototype.

--- modules/motion_matching/src/kdtree_wrapper.cpp
+++ modules/motion_matching/src/kdtree_wrapper.cpp
@@ -28,4 +28,87 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
 /**************************************************************************/
 
-#include "kdtree_wrapper.h"
+#include "kdtree_wrapper.h" PackedInt32Array KDTree::range_nearest_neighbors(PackedFloat32Array point, int64_t k) {
+using namespace Kdtree;
+auto begin = point.ptrw(), end = point.ptrw(); // We use the ptr as iterator.
+end = std::next(end, point.size());
+std::vector<float> p(begin, end);
+KdNodeVector result{};
+kd->range_nearest_neighbors(p, k, &result);
+PackedInt32Array indexes{};
+for (Kdtree::KdNode node : result) {
+	indexes.append(node.index);
+}
+return indexes;
+}
+PackedInt32Array KDTree::k_nearest_neighbors(PackedFloat32Array point, int64_t k) {
+	if (kd == nullptr) {
+		print_line("tree is null");
+		return {};
+	}
+	using namespace Kdtree;
+	auto begin = point.ptrw(), end = point.ptrw(); // We use the ptr as iterator.
+	end = std::next(end, point.size());
+	std::vector<float> p(begin, end);
+	KdNodeVector result{};
+	std::chrono::time_point clock_start = std::chrono::system_clock::now();
+	kd->k_nearest_neighbors(p, k, &result);
+	std::chrono::time_point clock_now = std::chrono::system_clock::now();
+	float currentTime = float(std::chrono::duration_cast<std::chrono::microseconds>(clock_now - clock_start).count());
+
+	print_line("Searching Data tooks ", currentTime, " ms");
+
+	PackedInt32Array indexes{};
+	for (Kdtree::KdNode node : result) {
+		indexes.append(node.index);
+	}
+	return indexes;
+}
+void KDTree::_bind_methods() {
+	using namespace godot;
+	ClassDB::bind_method(D_METHOD("set_dimension", "dimensionality"), &KDTree::set_dimension);
+
+	ClassDB::bind_method(D_METHOD("bake_nodes", "points"), &KDTree::bake_nodes);
+
+	ClassDB::bind_method(D_METHOD("set_weight", "weights"), &KDTree::set_weight);
+
+	ClassDB::bind_method(D_METHOD("k_nearest_neighbors", "points", "k"), &KDTree::k_nearest_neighbors);
+	ClassDB::bind_method(D_METHOD("range_nearest_neighbors", "points", "k"), &KDTree::range_nearest_neighbors);
+}
+void KDTree::bake_nodes(const PackedFloat32Array &points, int64_t dimensions) {
+	using namespace Kdtree;
+
+	print_line(vformat("KDTree baking data %d", dimensions, points.size()));
+
+	if ((points.size() % dimensions) != 0) {
+		print_line("points size not divisible by dimensions");
+		return;
+	}
+
+	KdNodeVector nodes{};
+	for (int64_t i = 0; i < points.size() / dimensions; ++i) {
+		auto begin = points.ptr(), end = points.ptr(); // We use the ptr as iterator.
+		begin = std::next(begin, dimensions * i);
+		end = std::next(begin, dimensions);
+		std::vector<float> point(begin, end);
+		nodes.push_back(Kdtree::KdNode(point, nullptr, i));
+		// u::prints(std::distance(points.ptrw(),begin),point.size());
+	}
+	print_line(points);
+	print_line("Nb poses", (int64_t)nodes.size());
+	kd = new Kdtree::KdTree(&nodes, 2);
+	print_line("Searching");
+	KdNodeVector re{};
+	kd->k_nearest_neighbors(std::vector<float>(points.ptr(), std::next(points.ptr(), dimensions)), 1, &re);
+	print_line(vformat("Found result %d", (int64_t)re.size()));
+}
+void KDTree::set_dimension(int dim) {
+	if (dim > 0) {
+		kd->dimension = (int64_t)dim;
+	}
+}
+void KDTree::set_weight(int difference_type, PackedFloat32Array weight) {
+	std::vector<float> w(&weight[0], &weight[weight.size()]);
+
+	kd->set_distance(difference_type, &w);
+}
