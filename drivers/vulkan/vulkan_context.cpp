509a395a31ef7dab3bb5303e27e2fbdb0a3e46ac
Pedro J. Estébanez <pedrojrulez@gmail.com>
2023-08-21 09:52:29 +0200

    Enhance Vulkan PSO caching

--- drivers/vulkan/vulkan_context.cpp
+++ drivers/vulkan/vulkan_context.cpp
@@ -503,6 +503,7 @@ Error VulkanContext::_initialize_device_extensions() {
 	register_requested_device_extension(VK_KHR_16BIT_STORAGE_EXTENSION_NAME, false);
 	register_requested_device_extension(VK_KHR_IMAGE_FORMAT_LIST_EXTENSION_NAME, false);
 	register_requested_device_extension(VK_KHR_MAINTENANCE_2_EXTENSION_NAME, false);
+	register_requested_device_extension(VK_EXT_PIPELINE_CREATION_CACHE_CONTROL_EXTENSION_NAME, false);
 
 	// TODO consider the following extensions:
 	// - VK_KHR_spirv_1_4
