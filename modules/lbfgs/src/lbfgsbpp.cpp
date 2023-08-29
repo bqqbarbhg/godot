385420859fb4f42d028aacb1472630bc6e4e6f59
K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>
2023-08-28 22:34:34 -0700

    Refactor documentation for improved readability and clarity

--- modules/lbfgs/src/lbfgsbpp.cpp
+++ modules/lbfgs/src/lbfgsbpp.cpp
@@ -43,9 +43,9 @@ Array LBFGSBSolver::minimize(const TypedArray<double> &p_x,
 	Eigen::VectorXd upper_bound = godot_to_eigen(p_upper_bound);
 
 	double fx = p_fx;
-	int niter = solver.minimize(operator_pointer, x, fx, lower_bound, upper_bound);
+	int number_of_iterations = solver.minimize(operator_pointer, x, fx, lower_bound, upper_bound);
 	Array ret;
-	ret.push_back(niter);
+	ret.push_back(number_of_iterations);
 	ret.push_back(eigen_to_godot(x));
 	ret.push_back(fx);
 	return ret;
@@ -56,36 +56,39 @@ LBFGSBSolver::LBFGSBSolver() {
 }
 
 void LBFGSBSolver::_bind_methods() {
-	GDVIRTUAL_BIND(_call_operator, "x", "grad");
-	ClassDB::bind_method(D_METHOD("minimize", "x", "fx"), &LBFGSBSolver::minimize);
+	GDVIRTUAL_BIND(_call_operator, "x", "gradient");
+	ClassDB::bind_method(D_METHOD("minimize", "x", "fx", "lower_bound", "upper_bound"), &LBFGSBSolver::minimize);
 }
 
-double LBFGSBSolver::native_operator(const Eigen::VectorXd &r_x, Eigen::VectorXd &r_grad) {
+double LBFGSBSolver::native_operator(const Eigen::VectorXd &r_x, Eigen::VectorXd &r_gradient) {
 	TypedArray<double> x = LBFGSBSolver::eigen_to_godot(r_x);
-	TypedArray<double> grad = LBFGSBSolver::eigen_to_godot(r_grad);
-	Eigen::VectorXd vec(r_grad.size());
-	for (int i = 0; i < r_grad.size(); ++i) {
-		vec[i] = r_grad[i];
+	TypedArray<double> gradient = LBFGSBSolver::eigen_to_godot(r_gradient);
+	Eigen::VectorXd vec(r_gradient.size());
+	for (int i = 0; i < r_gradient.size(); ++i) {
+		vec[i] = r_gradient[i];
 	}
-	double fx = call_operator(x, grad);
-	r_grad = godot_to_eigen(grad);
+	double fx = call_operator(x, gradient);
+	r_gradient = godot_to_eigen(gradient);
 	return fx;
 }
-double LBFGSBSolver::call_operator(const TypedArray<double> &p_x, TypedArray<double> &r_grad) {
+
+double LBFGSBSolver::call_operator(const TypedArray<double> &p_x, TypedArray<double> &r_gradient) {
 	double ret = 0;
-	if (GDVIRTUAL_CALL(_call_operator, p_x, r_grad, ret)) {
+	if (GDVIRTUAL_CALL(_call_operator, p_x, r_gradient, ret)) {
 		return ret;
 	};
 	return 0;
 }
-Eigen::VectorXd LBFGSBSolver::godot_to_eigen(const TypedArray<double> &p_array) {
-	ERR_FAIL_COND_V(!p_array.size(), Eigen::VectorXd());
-	Eigen::VectorXd vector(p_array.size());
-	for (int i = 0; i < p_array.size(); ++i) {
-		vector[i] = p_array[i];
+
+Eigen::VectorXd LBFGSBSolver::godot_to_eigen(const TypedArray<double> &p_packed_array) {
+	ERR_FAIL_COND_V(!p_packed_array.size(), Eigen::VectorXd());
+	Eigen::VectorXd vector(p_packed_array.size());
+	for (int i = 0; i < p_packed_array.size(); ++i) {
+		vector[i] = p_packed_array[i];
 	}
 	return vector;
 }
+
 TypedArray<double> LBFGSBSolver::eigen_to_godot(const Eigen::VectorXd &p_vector) {
 	ERR_FAIL_COND_V(!p_vector.size(), TypedArray<double>());
 	int size = p_vector.size();
