041fa2b2e6f0d5728247d83b54c1d514b7acdc68
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-09-27 20:51:52 -0700

    git subrepo clone --branch=main https://github.com/V-Sekai/godot-subdiv.git modules/subdiv
    subrepo:
      subdir:   "modules/subdiv"
      merged:   "602795b0ce"
    upstream:
      origin:   "https://github.com/V-Sekai/godot-subdiv.git"
      branch:   "main"
      commit:   "602795b0ce"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "110b9eb"

---
+++ modules/subdiv/config.py
@@ -0,0 +1,25 @@
+def can_build(env, platform):
+    return True
+
+
+def configure(env):
+    pass
+
+
+def get_doc_classes():
+    return [
+        "TriangleSubdivider",
+        "TopologyDataMesh",
+        "TopologyDataImporter",
+        "SubdivisionServer",
+        "SubdivisionMesh",
+        "SubdivisionBaker",
+        "Subdivider",
+        "QuadSubdivider",
+        "BakedSubdivMesh",
+        "QuadSubdivider",
+    ]
+
+
+def get_doc_path():
+    return "doc_classes"
