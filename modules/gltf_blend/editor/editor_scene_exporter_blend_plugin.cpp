bbf423b270f78efa5abf835f70e2b6c24715c730
K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>
2023-10-03 12:49:48 -0700

    Format scene exporter plugin for GLTF blend format

--- modules/gltf_blend/editor/editor_scene_exporter_blend_plugin.cpp
+++ modules/gltf_blend/editor/editor_scene_exporter_blend_plugin.cpp
@@ -130,7 +130,7 @@ void SceneExporterBlendPlugin::_gltf2_dialog_action(String p_file) {
 		print_error("Blender returned " + itos(ret));
 		return;
 	}
-	const Vector<String> p_files = {sink_global};
+	const Vector<String> p_files = { sink_global };
 	EditorFileSystem::get_singleton()->reimport_files(p_files);
 }
 
